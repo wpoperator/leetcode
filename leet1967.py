c7052b951d6791dd69a6ba8e5144cd1fa37bedd8
Robert Anderson <pwmrrob@outlook.com>
2024-05-22 16:59:32 -0400

    Leetcode 1967 complete

---
+++ leet1967.py
@@ -0,0 +1,17 @@
+# Leetcode quation 1967: Number of strings that appear as substrings in word
+
+def numOfStrings(patterns: list[str], word: str) -> int:
+    # count the number of substrings found
+    count = 0
+    # loop through patterns
+    for s in patterns:
+        if s in word:
+            # if s is found in word increment count
+            count += 1
+    return count
+
+
+# Test Cases
+print(numOfStrings(["a", "b", "c"], "aaaaabbbbb"))  # 2
+print(numOfStrings(["a", "abc", "bc", "d"], "abc"))  # 3
+print(numOfStrings(["a", "a", "a"], "ab"))  # 3
